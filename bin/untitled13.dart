2d6f90943383efd1afc3f1b7b8007dd48d56fc9f
Ahmadullo2023 <ahmadullo2023@gmail.com>
2023-05-29 17:54:05 +0500

    firstspp

---
+++ bin/untitled13.dart
@@ -0,0 +1,88 @@
+
+//1
+// write a function that tries to send an email. if the email cannot be sent, throw an exception
+
+
+// void validation(String str){
+//   final regex = RegExp(r'^[a-zA-Z0-9]{6,} @gmail.com$');
+//   if(regex.hasMatch(str)){
+//     throw Exception("Error email");
+//   }
+// }
+// void main(){
+//   String str = 'ahmadullo@gmail.com';
+//   try{
+//     validation(str);
+//     print("Validation success");
+//   }catch (e) {
+//     print("Eror not faound validation!");
+//   }
+// }
+
+
+
+//2
+// write a function that tries to sort a list of numbers. if the list is empty, throw an exception
+// напишите функцию, которая пытается отсортировать список чисел. если список пуст, сгенерировать исключение
+
+//   void sorting(List list){
+//     if(list.isNotEmpty){
+//       list.sort();
+//       print(list);
+//     }else{
+//       throw Exception("list empty");
+//     }
+// }
+// void main(){
+//   List list =[4,6,9,2,5,7,2];
+//   try{
+//     sorting(list);
+//     print("Validation success");
+//   }catch (e) {
+//     print("list empty");
+//   }
+// }
+
+
+
+//3
+// write a function that tries to parse a JSON string into a map. If the string is not valid JSON throw an exception.
+// If the JSON string does not contain a "name" property, throw an exception
+// напишите функцию, которая пытается преобразовать строку JSON в карту. Если строка недействительна, JSON выдает исключение.
+// Если строка JSON не содержит свойства «имя», создайте исключение.
+
+
+
+
+//4
+// Write a function that tries to connect to a database and return the connection object. If the connection fails, throw an exaption.
+// If the connection is closed, throw an exaption. If the connection is closed, thow an exception
+// Напишите функцию, которая пытается подключиться к базе данных и возвращает объект подключения. Если соединение не удается, выдайте exaption.
+// Если соединение закрыто, кидайте exaption. Если соединение закрыто, создайте исключение
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
